0c07dd1220b44ccf5ba6ea64577b50521952dac7
varunm2004 <varunmenon2004@gmail.com>
2024-02-20 15:15:55 -0500

    Flask import and html setup

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
